016c07f4775ced901f07ea46787cdad8c2d78f74
yahyaamirudin <yahyanasrulloh98@gmail.com>
2019-12-08 20:51:15 +0700

    First commit-Tampil team dan detail pemain fix

---
+++ js/api.js
@@ -0,0 +1,149 @@
+function status(response) {
+  if (response.status !== 200) {
+    console.log("Error : " + response.status);
+    return Promise.reject(new Error(response.statusText));
+  } else {
+    return Promise.resolve(response);
+  }
+}
+
+function json(response) {
+  return response.json();
+}
+
+function error(error) {
+  console.log("Error : " + error);
+}
+
+function getKlasemen() {
+
+  fetch("https://api.football-data.org/v2/competitions/2002/standings?standingType=TOTAL", {
+      method: "GET",
+      headers: {
+        "X-Auth-Token": "590c7b06bf5d4a7ca5a5d73e4d84645a"
+      }
+    }).then(status).then(json).then(function (data) {
+      // console.log(data)
+      let klasemenHTML = "";
+      let dataTeamHTML = "";
+      data.standings.forEach(function (a_standing) {
+        a_standing.table.forEach(function (a_team) {
+
+          dataTeamHTML +=
+            `<tr>
+            <td class="center-align">${a_team.position}</td>
+            <td>
+            <a href="./detail.html?id=${a_team.team.id}">
+            <p class="hide-on-small-only">
+            <img class = "show-on-medium-and-up show-on-medium-and-down" src=${a_team.team.crestUrl} style="float:left;width:22px;height:22px;margin-right:20px">
+            ${a_team.team.name}
+            </p>
+            <p class="hide-on-med-and-up">
+            <img src=${a_team.team.crestUrl}  style="float:left;width:22px;height:22px;margin-right:20px">
+            </p>
+  
+            </a>
+            </td>
+            <td class="center-align">${a_team.playedGames}</td>
+            <td class="center-align">${a_team.won}</td>
+            <td class="center-align">${a_team.draw}</td>
+            <td class="center-align">${a_team.lost}</td>
+            <td class="center-align">${a_team.goalsFor}</td>
+            <td class="center-align">${a_team.goalsAgainst}</td>
+            <td class="center-align">${a_team.goalDifference}</td>
+             <td class="center-align">${a_team.points}</td>
+          </tr>`
+        });
+        klasemenHTML += `
+      <div class="row">
+      <div class="col s12 m12" id="tabelKlasmen">
+        <div class="card">
+          <div class="card-content">
+            <table class="responsive-table striped ">
+              <thead>
+                <tr>
+                  <th class="center-align">Position</th>
+                  <th>Team</th>
+                  <th class="center-align">Play</th>
+                  <th class="center-align">Won</th>
+                  <th class="center-align">Draw</th>
+                  <th class="center-align">Lost</th>
+                  <th class="center-align">GF</th>
+                  <th class="center-align">GA</th>
+                  <th class="center-align">GD</th>
+                  <th class="center-align">Points</th>
+                </tr>
+              </thead>
+              <tbody>
+                ${dataTeamHTML}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>         
+        `
+      });
+      document.getElementById("body-content").innerHTML = klasemenHTML
+    })
+    .catch(error);
+}
+
+function getDetailTeam() {
+  var urlParams = new URLSearchParams(window.location.search);
+  var idParam = urlParams.get("id");
+  fetch(`https://api.football-data.org/v2/teams/${idParam}`, {
+      method: "GET",
+      headers: {
+        "X-Auth-Token": "590c7b06bf5d4a7ca5a5d73e4d84645a"
+      }
+    }).then(status).then(json).then(function (data) {
+      console.log(data);
+      let squadHTML = "";
+      let daftarHTML = "";
+      // data.result.forEach(tabel => {
+      data.squad.forEach(pemain => {
+        squadHTML += `
+            <tr>
+              <td>
+              <p class="hide-on-small-only">
+          ${pemain.name}
+          </p>
+              </td>
+              <td>${pemain.position}</td>
+              <td>${pemain.dateOfBirth}</td>
+              <td>${pemain.countryOfBirth}</td>
+              <td>${pemain.nationality}</td>
+              <td>${pemain.role}</td>\
+            </tr>
+          `
+      });
+      daftarHTML += `
+        <div class="row">
+          <div class="col s12 m12" id="tabelKlasmen">
+            <div class="card">
+              <div class="card-content">
+                <table class="responsive-table striped ">
+                  <thead>
+                    <tr>
+                      <th>Nama</th>
+                      <th>Posisi</th>
+                      <th>Tanggal lahir</th>
+                      <th>Tempat lahir</th>
+                      <th>Kewarganegaraan</th>
+                      <th>Role</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    ${squadHTML}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>`
+      // });
+      document.getElementById("body-content").innerHTML = daftarHTML;
+    })
+    .catch(error);
+}
---
+++ service-worker.js
@@ -0,0 +1,81 @@
+const CACHE_NAME = "Updatebola";
+var urlsToCache = [
+  "/",
+  "/nav.html",
+  "/index.html",
+  "/detail.html",
+  "/pages/klasemen.html",
+  "/pages/jadwal.html",
+  "/pages/favorit.html",
+  "/css/materialize.min.css",
+  "/js/api.js",
+  "/js/materialize.min.js",
+  "/js/nav.js"
+];
+ 
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// mengambil asset 
+// self.addEventListener("fetch", function(event) {
+//     event.respondWith(
+//       caches
+//         .match(event.request, { cacheName: CACHE_NAME })
+//         .then(function(response) {
+//           if (response) {
+//             console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+//             return response;
+//           }
+   
+//           console.log(
+//             "ServiceWorker: Memuat aset dari server: ",
+//             event.request.url
+//           );
+//           return fetch(event.request);
+//         })
+//     );
+//   });
+self.addEventListener("fetch", event => {
+  let base_url = "https://api.football-data.org/v2";
+  if (event.request.url.indexOf(base_url) > -1) {
+    event.respondWith(
+      (async () => {
+        const cache = await caches.open(CACHE_NAME);
+        const res = await fetch(event.request);
+        cache.put(event.request.url, res.clone());
+        return res;
+      })()
+    );
+  } else {
+    event.respondWith(
+      (async () => {
+        const res = await caches.match(event.request.url, {
+          ignoreSearch: true
+        });
+        return res || (await fetch(event.request));
+      })()
+    );
+  }
+});
+
+  
+//hapus storage
+self.addEventListener("activate", function(event) {
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.map(function(cacheName) {
+            if (cacheName != CACHE_NAME) {
+              console.log("ServiceWorker: cache " + cacheName + " dihapus");
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
